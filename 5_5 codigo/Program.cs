af749a08426d497494e7332b16d52d6f2e2a9680
gregorycesarq <cesargregory468@gmail.com>
2025-07-06 17:29:54 -0400

    Add files via upload

---
+++ 5_5 codigo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        EjemploConjunto();
+    }
+
+    static void EjemploConjunto()
+    {
+        HashSet<string> frutas = new HashSet<string>();
+
+        frutas.Add("Manzana");
+        frutas.Add("Banana");
+        frutas.Add("Naranja");
+        frutas.Add("Manzana"); // No se repite
+
+        Console.WriteLine("Frutas en el conjunto:");
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+
+        if (frutas.Contains("Banana"))
+        {
+            Console.WriteLine("\nEl conjunto contiene Banana.");
+        }
+
+        if (frutas.Contains("Uva"))
+        {
+            Console.WriteLine("El conjunto contiene Uva.");
+        }
+        else
+        {
+            Console.WriteLine("El conjunto NO contiene Uva.");
+        }
+    }
+}
